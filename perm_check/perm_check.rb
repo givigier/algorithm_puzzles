a3908766456333aa367e7b278f53392d5edee2b7
Gabriel Givigier <gabriel@givigier.com>
2016-10-04 22:53:39 -0300

    Add perm_check

--- perm_check/perm_check.rb
+++ perm_check/perm_check.rb
@@ -1,9 +1,8 @@
 class PermCheck
   def check(a)
     array_length = a.length
-    uniq_length = a.uniq.length
     return 0 if a.min != 1
-    return 0 if array_length != uniq_length
+    return 0 if array_length != a.uniq.length
     return 0 if a.max != array_length
     1
   end
